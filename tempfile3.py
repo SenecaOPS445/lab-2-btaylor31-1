7342476e20630a8f8cec453785786255232c2969
Bakari Taylor <btaylor31@myseneca.ca>
2025-01-24 14:15:44 -0500

    updates

---
+++ tempfile3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env
+
+a = 17
+b = 13
+if a > b:
+    print('a is greater than b')
+elif a < b:
+    print('b is greater than a')
+else:
+    print('a is not greater than b')
+    print('a is not less than b')
+    print('Therefore, a is equal to b')
