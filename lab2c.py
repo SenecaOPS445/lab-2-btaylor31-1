bf67d0e5564e3249f46df32c783be87f3e70a43b
Bakari Taylor <btaylor31@myseneca.ca>
2025-01-24 14:16:48 -0500

    made changes

--- lab2c.py
+++ lab2c.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
-name = input ("Name: ")
-age = input ("Age: ")
-print('Hi ' + name + ', you are ' + str(age) + ' years old.')
+import sys
+
+arguments = sys.argv
+script = sys.argv[0]
+name = sys.argv[1]
+age = sys.argv[2]
+
+print('Hi ' + sys.argv[1] + ', you are ' + sys.argv[2] + ' years old.')
+
